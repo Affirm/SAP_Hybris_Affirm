0522bfd92bb889f3f3b81942d79016092352a9df
Ryan McNicol <ryan.mcnicol@affirm.com>
2022-03-29 15:21:43 -0700

    rm parent dir

--- bin/modules/affirm/affirmpaymentcore/src/com/affirm/payment/service/executor/AffirmPaymentServiceExecutor.java
+++
@@ -1,9 +0,0 @@
-package com.affirm.payment.service.executor;
-
-import com.affirm.payment.service.executor.request.AffirmPaymentServiceRequest;
-import com.affirm.payment.service.executor.result.AffirmPaymentServiceResult;
-
-public interface AffirmPaymentServiceExecutor {
-   AffirmPaymentServiceResult execute(final AffirmPaymentServiceRequest request);
-}
-
